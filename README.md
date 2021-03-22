3122af6a0a0a3434db3297596c764191612e9605
V31l <carlo.meier@mail.ch>
2021-03-22 11:38:47 +0100

    cli commands added

--- README.md
+++ README.md
@@ -1,38 +1,22 @@
-# node-ipns-pin
+# ipns-pin
 
 A small script that updates pins for ipns addresses. Should be set up as a cron job.
 
 ## Requirements
-- basic knowledge of the cli
-- know how to read `json`
-- `npm`/`yarn`
 - a running ipfs node
 
-## Instructions
+## Usage
 
-### First time
-- run `npm install` or `yarn` to install dependencies
-- copy `config.example.json` to `config.json`
+Note: *You'll probably want to set up a cron job for the update command*
 
-### General
-`node index.js`
+|Command|Description
+|---|---
+|ipns-pin update|Updates pins
+|ipns-pin ls|lists all pins
+|ipns-pin add {nodeid/dnslink}|add new ipns pin
+|ipns-pin rm {nodeid/dnslink}|remove a ipns pin
+|ipns-pin ipfs {api address}|set which ipfs node to use
 
-### Configuration
-Configuration is done through the `config.json` file.
-To change or set which ipfs node edit the `ipfs` value in the json file.
-Valid formats are described [here](https://github.com/ipfs/js-ipfs/tree/master/packages/ipfs-http-client#ipfshttpclientoptions).
 
-**Add Pins:**
-To add new ipns pins add them to the pins list like this:
-```json
-{
-  "ipns": "full.domain.example"
-}
-```
-*instead of a domain name you can also use a peer id*
-
-**Remove pins:** To remove a pin just remove the entry from the list.
-If you want the current version unpinned to youre goind to have to manually unpin the cid from the json file.
-`ipfs pin rm {QmHash/bafyHash}`
-
-*If I use this thing enough I'll probably add cli commands to add/remove pins.*
+~~If I use this thing enough I'll probably add cli commands to add/remove pins.~~
+Boredom also works
--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const ipfsApi = require('ipfs-http-client')
 const fs = require('fs')
 
 const configFile = './config.json'
+const argv = process.argv.slice(2)
+
+let cfg = {
+	ipfs: "http://localhost:5001/api/v0",
+	pins: [],
+}
 
 // Load config file
-const cfg = require(configFile)
+try {
+    cfg = {...cfg, ...(JSON.parse(fs.readFileSync(configFile, 'utf8')))}
+} catch (e) {
+    console.log("Warning: no config.json file found. using defaults")
+}
+
 
 // Connect to ipfs node
 const ipfs = new ipfsApi(cfg.ipfs)
@@ -37,16 +48,72 @@ const updatePin = (pin) => new Promise(async (resolve, reject) => {
     }
 })
 
-// If pins exist run them all through the previous helper
-if (cfg.pins) {
-    console.log('Updating pins...')
-    Promise.all(cfg.pins.map(updatePin)).then(r => {
-        console.log('Pins updated', r)
-        // Merge updated pins with existing config
-        const newCfg = {...cfg, pins: r}
-
-        console.log('Writing config...')
-        fs.writeFileSync(configFile, JSON.stringify(newCfg, null, '\t'))
-        console.log('Done!')
-    })
+const addPin = (ipns) => {
+    const add = cfg.pins.find(p => p.ipns == ipns)
+    if (add)
+        console.log("Error: pin already exists")
+    else {
+        writeConfig({...cfg, pins: [...cfg.pins, {ipns: ipns}]})
+    }
 }
+
+const rmPin = async (ipns) => {
+    const rm = cfg.pins.find(p => p.ipns == ipns)
+    if (rm) {
+        if (rm.current)
+            await ipfs.pin.rm(rm.current)
+        writeConfig({...cfg, pins: cfg.pins.filter(p => p.ipns != ipns)})
+    } else {
+        console.log("Error: pin not found")
+    }
+}
+
+const listPins = () => {
+    for (const pin of cfg.pins) {
+        console.log(`${pin.ipns} ${pin.current}`)
+    }
+}
+
+const writeConfig = (cfg) => {
+    console.log('Writing config...')
+    fs.writeFileSync(configFile, JSON.stringify(cfg, null, '\t'))
+    console.log('Done!')
+}
+
+const printHelp = () => {
+    console.log("Usage:")
+    console.log("ipns-pin update               - Updates pins")
+    console.log("ipns-pin ls                   - lists all pins")
+    console.log("ipns-pin add {nodeid/dnslink} - add new ipns pin")
+    console.log("ipns-pin rm {nodeid/dnslink}  - remove a ipns pin")
+    console.log("ipns-pin ipfs {api address}   - set which ipfs node to use")
+}
+
+// If pins exist run them all through the previous helper
+if (argv.length == 1) {
+    if (argv[0] == "update") {
+        if (cfg.pins) {
+            console.log('Updating pins...')
+            Promise.all(cfg.pins.map(updatePin)).then(r => {
+                console.log('Pins updated', r)
+                // Merge updated pins with existing config
+                const newCfg = {...cfg, pins: r}
+                writeConfig(newCfg)
+            })
+        } else {
+            console.log('error: no pins found in config.json')
+        }
+    }
+   else if (argv[0] == "ls") {
+        listPins()
+   } else printHelp()
+} else if (argv.length == 2) {
+    if (argv[0] == "add") {
+        addPin(argv[1])
+    } else if (argv[0] == "rm") {
+        rmPin(argv[1])
+    } else if (argv[0] == 'ipfs') {
+        writeConfig({...cfg, ipfs: argv[1]})
+    } else printHelp()
+} else printHelp()
+
